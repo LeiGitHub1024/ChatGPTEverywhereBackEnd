ee822d3015e67c903fee40530e2bc10c18678815
LeiGitHub1024 <cmcc.coder@gmail.com>
2023-02-20 22:09:48 +0800

    1

---
+++ pages/api/chatgpt_alyosha.js
@@ -0,0 +1,26 @@
+import { ChatGPTAPI } from 'chatgpt'
+
+
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if(req.method=='POST'){
+    res.send({"res":'post接口还没准备好'})
+  }else{
+    let key = req.query.gptkey;
+    let question = req.query.question;
+    console.log(key,question)
+    if(!key){res.send({'res':'Request Faild: API key not found, please click on the settings button above to configure.'});return;}
+    if(!question){res.send({'res':'Request Faild: No quesetion detected.'});return }  
+    try{
+      let api = new ChatGPTAPI({
+        apiKey: key
+      })
+      let gptanswer = await api.sendMessage(question)
+      res.send({"res":gptanswer.text})
+    }catch{
+      res.send({'res':'Request Faild: Incorrect API key provided: key. You can find your API key at https://platform.openai.com/account/api-keys.'})
+    }
+  }
+}
